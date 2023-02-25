df24a8e1fb4d9350adc831a702e57d16c10d9972
Michal Grňo <m93a.cz@gmail.com>
2023-02-25 23:07:23 +0100

    thead & tfoot

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -15,6 +15,19 @@
 	let table: HTMLTableElement = undefined as any;
 	export { table as this };
 
+	export let headStyle: string = '';
+	export let headId: string | undefined = undefined;
+	export let headClass: string | undefined = undefined;
+	export let thead: HTMLTableSectionElement = undefined as any;
+	export let bodyStyle: string = '';
+	export let bodyId: string | undefined = undefined;
+	export let bodyClass: string | undefined = undefined;
+	export let tbody: HTMLTableSectionElement = undefined as any;
+	export let footStyle: string = '';
+	export let footId: string | undefined = undefined;
+	export let footClass: string | undefined = undefined;
+	export let tfoot: HTMLTableSectionElement = undefined as any;
+
 	// output props
 	export let computedColumnWidths: number[] = [];
 	export let computedRowHeights: number[] = [];
@@ -53,9 +66,19 @@
 </script>
 
 <table bind:this={table} style={allStyles} {id} class={klass} class:ssr={!browser}>
-	<tbody>
+	{#if $$slots.head}
+		<thead bind:this={thead} style={headStyle} id={headId} class={headClass}>
+			<slot name="head" />
+		</thead>
+	{/if}
+	<tbody bind:this={tbody} style={bodyStyle} id={bodyId} class={bodyClass}>
 		<slot />
 	</tbody>
+	{#if $$slots.foot}
+		<tfoot bind:this={tfoot} style={footStyle} id={footId} class={footClass}>
+			<slot name="foot" />
+		</tfoot>
+	{/if}
 </table>
 
 <style lang="scss">
